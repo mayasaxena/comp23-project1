84d31412950ff0d7afcdb0f77aaa26fc6f783aec
Maya Saxena <maya.saxena@tufts.edu>
2015-09-29 20:49:59 -0400

    Fixed collisions

--- js/level1risky.js
+++ js/level1risky.js
@@ -1,58 +1,56 @@
-var playerSpeed = 1;
 var tileSize = 32;
-var backgroundX = 160;
-var backgroundY = 220;
+var tweenTime = 250;
+var backgroundX = 0;
+var backgroundY = 0;
 
 function Level1Risky() {
     this.player;
+    this.map;
     this.chairs;
     this.cursors;
     this.xCoord;
     this.yCoord;
     this.destination;
     this.size = { x: 32, y: 32 };
-    this.speed = playerSpeed;
     this.moveIntention;
     this.isSnapping;
+    this.obstacles;
 };
 
 Level1Risky.prototype = {
     preload: function() {
         console.log("Risky but Rewarding");
-        this.game.load.image('background', 'assets/images/map1.png');
         this.game.load.image('chair', 'assets/images/risky_chair.png');
         this.game.load.image('tree', 'assets/images/risky_tree.png');
         this.game.load.image('player', 'assets/images/playersprite.png');
-        // load all game assets
-        // images, spritesheets, atlases, audio etc..
+
+        this.game.load.tilemap('background', 'assets/tilemaps/maps/risky.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/tilemaps/tiles/risky_tileset.png');
     },
 
-    // var chairs;
     create: function() {
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.map = this.game.add.tilemap('background');
+        this.map.addTilesetImage('risky_tileset', 'tiles');
+    
+        var floors = this.map.createLayer("Floor");
+        var objects = this.map.createLayer("Objects");
+        this.obstacles = this.map.createLayer("Collisions");
 
-        this.game.add.sprite(backgroundX, backgroundY, 'background');
-        
-        this.chairs = this.game.add.group();
-        this.chairs.enableBody = true;
-        var chair = this.chairs.create(backgroundX + 1 * 32, backgroundY + 4, 'chair');
-        chair.body.immovable = true;
-        chair = this.chairs.create(backgroundX + 3 * 32, backgroundY + 4, 'chair');
-        chair.body.immovable = true;
+        this.map.setCollisionByExclusion([2], true, this.obstacles);
+        this.map.setCollision(0, true, this.obstacles);
 
-
-        this.player = this.game.add.sprite(backgroundX, backgroundY + 2 * 32, 'player');
+        this.player = this.game.add.sprite(0, tileSize * 2, 'player');
         this.game.physics.arcade.enable(this.player);
         this.player.body.collideWorldBounds = true;
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
         this.destination = null;
-
     },
+
     update: function() {
 
-        this.game.physics.arcade.collide(this.player, this.chairs);
+        this.game.physics.arcade.collide(this.player, this.obstacles);
 
         this.xCoord = Math.floor(((this.player.x + tileSize / 2) - backgroundX) / tileSize);
         this.yCoord = Math.floor(((this.player.y + tileSize / 2) - backgroundY) / tileSize);
@@ -75,25 +73,25 @@ Level1Risky.prototype = {
                 this.stopMoving();
             }
             // Stop the moving entity when it hits a wall.
-            else if(this.isMoving() && this.justReachedDestination() && this.moveIntention) {
+            else if(this.isMoving() && this.justReachedDestination() && this.moveIntention &&
+                    !this.canMoveDirectionFromTile(this.destination.x, this.destination.y, this.moveIntention)) {
                 this.stopMoving();
             }
             // Destination reached, but set new destination and keep going.
             else if(this.isMoving() && this.justReachedDestination() && this.moveIntention &&
+                    this.canMoveDirectionFromTile(this.destination.x, this.destination.y, this.moveIntention) &&
                     this.moveIntention === this.lastMove) {
                 this.continueMovingFromDestination();
             }
             // Destination reached, but changing direction and continuing.
             else if(this.isMoving() && this.justReachedDestination() && this.moveIntention &&
+                    this.canMoveDirectionFromTile(this.destination.x, this.destination.y, this.moveIntention) &&
                     this.moveIntention !== this.lastMove) {
                 this.changeDirectionAndContinueMoving(this.moveIntention);
             }
-            // Destination not yet reached, so keep going.
-            else if(this.isMoving() && !this.justReachedDestination()) {
-                this.continueMovingToDestination();
-            }
             // Not moving, but wanting to, so start!
-            else if(!this.isMoving() && this.moveIntention) {
+            else if(!this.isMoving() && this.moveIntention &&
+                    this.canMoveDirectionFromCurrentTile(this.moveIntention)) {
                 this.startMoving(this.moveIntention);
             }
         }
@@ -104,38 +102,32 @@ Level1Risky.prototype = {
     },
 
     getTileAdjacentToTile: function(tileX, tileY, direction) {
-        if(direction === moveType.UP) tileY += -1;
-        else if(direction === moveType.DOWN) tileY += 1;
-        else if(direction === moveType.LEFT) tileX += -1;
-        else if(direction === moveType.RIGHT) tileX += 1;
+        if (direction === moveType.UP) {
+            tileY += -1;
+        } else if(direction === moveType.DOWN) {
+            tileY += 1;
+        } else if(direction === moveType.LEFT) {
+            tileX += -1;
+        } else if(direction === moveType.RIGHT) {
+            tileX += 1;
+        } 
         return { x: tileX, y: tileY };
     },
 
     startMoving: function(direction) {
-        // Get current tile position.
         var currTile = this.getCurrentTile();
-        // Get new destination.
         this.destination = this.getTileAdjacentToTile(currTile.x, currTile.y, direction);
-
-        // Move.
-        this.setVelocityByTile(this.destination.x, this.destination.y, this.speed);
-        // Remember this move for later.
         this.lastMove = direction;
     },
 
-    continueMovingToDestination: function() {
-        this.setVelocityByTile(this.destination.x, this.destination.y, this.speed);
-    },
-
     continueMovingFromDestination: function() {
-        this.destination = this.getTileAdjacentToTile(this.destination.x, this.destination.y, this.lastMove);
-        this.setVelocityByTile(this.destination.x, this.destination.y, this.speed);
+        this.snapToTile(this.destination.x, this.destination.y);
+        this.destination = null;
     },
 
     changeDirectionAndContinueMoving: function(newDirection) {
         this.snapToTile(this.destination.x, this.destination.y);
         this.destination = this.getTileAdjacentToTile(this.destination.x, this.destination.y, newDirection);
-        this.setVelocityByTile(this.destination.x, this.destination.y, this.speed);
         this.lastMove = newDirection;
     },
 
@@ -149,7 +141,7 @@ Level1Risky.prototype = {
         this.isSnapping = true;
         var tween = this.game.add.tween(this.player).to({ x: x * tileSize + backgroundX, 
                                                           y: y * tileSize + backgroundY}, 
-                                                          320, 
+                                                          tweenTime, 
                                                           Phaser.Easing.Linear.None, 
                                                           true);
         tween.onComplete.add(this.stopSnap, this);
@@ -167,13 +159,20 @@ Level1Risky.prototype = {
         return this.destination !== null;
     },
 
-    // Sets the velocity of the entity so that it will move toward the tile.
-    setVelocityByTile: function(tileX, tileY, velocity) {
-        this.player.body.velocity.x = this.player.body.velocity.y = 0;
-        if(this.xCoord > tileX) this.player.body.velocity.x = -velocity;
-        else if(this.xCoord < tileX) this.player.body.velocity.x = velocity;
-        else if(this.yCoord > tileY) this.player.body.velocity.y = -velocity;
-        else if(this.yCoord < tileY) this.player.body.velocity.y = velocity;
+    canMoveDirectionFromTile: function(tileX, tileY, direction) {
+        var newPos = this.getTileAdjacentToTile(tileX, tileY, direction);
+        var tile = (this.map.getTile(newPos.x, newPos.y, this.obstacles));
+        console.log(tile);
+        if (tile) {
+            return !(this.map.collideIndexes.indexOf(tile.index) != -1);
+        } else {
+            return true;
+        }
+    },
+
+    canMoveDirectionFromCurrentTile: function(direction) {
+        var currTile = this.getCurrentTile();
+        return this.canMoveDirectionFromTile(currTile.x, currTile.y, direction);
     },
 
     stopSnap: function() {
