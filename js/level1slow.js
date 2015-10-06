2a41c87cbe9a1e8a5b4143418c890d5b60427556
eschreibman <eliza.schreibman@gmail.com>
2015-10-06 13:28:04 -0400

    created slow tile map, slow.json files

--- js/level1slow.js
+++ js/level1slow.js
@@ -1,29 +1,39 @@
 var tileSize = 32;
 var tweenTime = 250;
+var backgroundX = 0;
+var backgroundY = 0;
 
-function Level1Risky() {};
+function Level1Slow() {};
 
 Level1Risky.prototype = {
     preload: function() {
-        console.log("Risky but Rewarding");
-        this.game.load.image('chair', 'assets/images/risky_chair.png');
-        this.game.load.image('tree', 'assets/images/risky_tree.png');
+        console.log("Slow and steady");
         this.game.load.image('player', 'assets/images/playersprite.png');
 
-        this.game.load.tilemap('background', 'assets/tilemaps/maps/risky.json', null, Phaser.Tilemap.TILED_JSON);
-        this.game.load.image('tiles', 'assets/tilemaps/tiles/risky_tileset.png');
+        this.game.load.tilemap('background', 'assets/tilemaps/maps/slow.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/tilemaps/tiles/slow_tileset.png');
     },
 
     create: function() {
         this.map = this.game.add.tilemap('background');
-        this.map.addTilesetImage('risky_tileset', 'tiles');
+        this.map.addTilesetImage('slow_tileset', 'tiles');
     
-        var floors = this.map.createLayer("Floor");
-        var objects = this.map.createLayer("Objects");
-        this.obstacles = this.map.createLayer("Collisions");
+        this.floors = this.map.createLayer("Floor");
+        this.filecab = this.map.createLayer("File cabinets");
+        this.obstacles = this.map.createLayer("Walls");
 
-        this.map.setCollisionByExclusion([2], true, this.obstacles);
-        this.map.setCollision(0, true, this.obstacles);
+        this.map.setCollisionByExclusion([], true, this.obstacles);
+
+        this.player = new Player(this.game, tileSize, tileSize * 3);
+        this.player.map = this.map;
+        this.player.obstacles = this.obstacles;
+        this.player.handleDoor = this.handleDoor;
+
+        this.game.camera.bounds = null;
+        this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_TOPDOWN);
+        this.game.renderer.renderSession.roundPixels = true
+
+        //this.map.setCollision(0, true, this.obstacles);
 
         this.player = new Player(this.game, this.map, this.obstacles, 0, tileSize * 2);
     }
